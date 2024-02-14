59b5d3c08d462ab6a8fd808155dad50135e5caae
Vadim Volkov <vadimvolk@gmail.com>
2024-02-15 02:15:39 +0300

    Combined media + launcher layer + bootloader settings for flashing from command line

--- keyboards/nuphy/air75_v2/ansi/keymaps/vadimvolk/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/vadimvolk/keymap.c
@@ -16,32 +16,52 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "print.h"
 
 enum layers {
     L_MAIN,
-    L_MEDIA,
-    L_WINDOWS,
-    L_WINDOWS_MEDIA,
-    L_SIDE_CTL
+    L_MEDIA_LAUNCHER, // media control. all keys sent together with KC_LAUNCHER for intercept with Karabiner Elements
+    L_CONTROL, // keyboard control
+    // original layers for reference
+    // L_WINDOWS,
+    // L_WINDOWS_MEDIA,
+    // L_SIDE_CTL
 };
+
 
 enum my_keycodes {
     _KC_CLP_M = RGB_TEST + 1
 };
 
-const int LAYERS_SIZE = L_SIDE_CTL + 1;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // layer Mac
 [L_MAIN] = LAYOUT_ansi_84(
-	KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	MAC_PRT,	_KC_CLP_M,	KC_F13,
-	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 				KC_BSPC,	KC_HOME,
-	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 				KC_BSLS,	KC_END,
-	KC_CAPS,	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	 						KC_ENT,		KC_PGUP,
-	KC_LSFT,				KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,				KC_RSFT,	KC_UP,		KC_PGDN,
-	KC_LCTL,	KC_LALT,	KC_LGUI,										KC_SPC, 							KC_RGUI,	MO(1),   	KC_RCTL,				KC_LEFT,	KC_DOWN,    KC_RGHT),
+	KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  						KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	MAC_PRT,	_KC_CLP_M,	KC_F13,
+	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,    	KC_5,   	KC_6,   						KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 				KC_BSPC,	MO(L_CONTROL),
+	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   						KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 				KC_BSLS,	KC_END,
+	KC_CAPS,	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   						KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	 						KC_ENT,		KC_PGUP,
+	KC_LSFT,				KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   						KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,				KC_RSFT,	KC_UP,		KC_PGDN,
+	KC_LCTL,	KC_LALT,	KC_LGUI,										LT(L_MEDIA_LAUNCHER, KC_SPC), 							KC_RGUI,	KC_RALT,   	KC_RCTL,				KC_LEFT,	KC_DOWN,    KC_RGHT),
 
+[L_MEDIA_LAUNCHER] = LAYOUT_ansi_84(
+	_______, 	KC_BRID,  	KC_BRIU,  	MAC_TASK, 	KC_F13,		MAC_VOICE,  MAC_DND,    KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	MAC_PRTA,	_______,	_______,
+	_______, 	_______,  	_______,  	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 				_______,	_______,
+	_______, 	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 				_______,	_______,
+	_______,	_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______,   	_______,   	_______,  	_______,	_______, 	 						_______,	_______,
+	_______,				_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______, 	_______,	_______,	_______,				_______,	_______,	_______,
+	_______,	_______,	_______,										_______, 							_______,	_______,   	_______,				_______,	_______,    _______),
+
+[L_CONTROL] = LAYOUT_ansi_84(
+	_______, 	_______,  	_______,  	_______, 	_______,	_______,  	_______,    _______,  	_______,  	_______, 	_______, 	_______, 	_______, 	_______,	_______,	_______,
+	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 				_______,	_______,
+	_______, 	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	DEV_RESET,	SLEEP_MODE, 			BAT_SHOW,	_______,
+	_______,	_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______,   	_______,   	_______,  	_______,	_______, 	 						_______,	_______,
+	_______,				_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______, 	RGB_SPD,	RGB_SPI,	_______,				_______,	RGB_VAI,	_______,
+	_______,	_______,	_______,										_______, 							_______,	_______,   	_______,				RGB_MOD,	RGB_VAD,    RGB_HUI),
+
+/*
 // layer Mac Fn
 [L_MEDIA] = LAYOUT_ansi_84(
 	_______, 	KC_BRID,  	KC_BRIU,  	MAC_TASK, 	KC_F13,		MAC_VOICE,  MAC_DND,    KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	MAC_PRTA,	_______,	_______,
@@ -76,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______, 	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 				_______,	_______,
 	_______,	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,	_______, 	 						_______,	_______,
 	_______,				_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	SIDE_SPD,	SIDE_SPI,	_______,				_______,	SIDE_VAI,	_______,
-	_______,	_______,	_______,										_______, 							_______,	MO(4),   	_______,				SIDE_MOD,	SIDE_VAD,   SIDE_HUI)
+	_______,	_______,	_______,										_______, 							_______,	MO(4),   	_______,				SIDE_MOD,	SIDE_VAD,   SIDE_HUI)*/
 };
 
 /* qmk process record */
@@ -96,4 +116,28 @@ bool process_record_vadimvolk(uint16_t keycode, keyrecord_t *record) {
         default:
             return true;
     }
+}
+
+bool launcherSent = false;
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+	uprintf("layer_state_set_user %i\n", state);
+	switch (get_highest_layer(state)) {
+		case L_MEDIA_LAUNCHER:
+			println("Media layer activated");
+			register_code(KC_LAUNCHER);
+			launcherSent = true;
+			break;
+		case L_CONTROL:
+			println("Control layer activated");
+			break;
+		default:
+			if (launcherSent) {
+				launcherSent = false;
+				println("Media layer released");
+				unregister_code(KC_LAUNCHER);
+			}
+			break;
+	}
+	return state;
 }
